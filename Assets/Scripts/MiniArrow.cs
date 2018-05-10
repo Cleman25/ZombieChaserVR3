f3d978ca8c88e47173d2b1b782b67393aa0227a6
Clement Abel <clementan25@gmail.com>
2018-05-09 20:24:15 -0400

    Fixed the minimap: arrow and camera work better

---
+++ Assets/Scripts/MiniArrow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniArrow : MonoBehaviour {
+    public GameObject camera;
+	// Use this for initialization
+	void Start () {
+        camera = Camera.main.gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 rot = transform.eulerAngles;
+        rot.y = camera.transform.eulerAngles.y;
+        transform.rotation = Quaternion.Euler(rot);
+
+	}
+}
