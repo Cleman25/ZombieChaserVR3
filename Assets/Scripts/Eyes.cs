7c80ffcfb709267590cc44c7af3676a4a9d6e897
Clement Abel <clementan25@gmail.com>
2018-05-15 21:24:42 -0400

    Zombie Eyes script

---
+++ Assets/Scripts/Eyes.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Eyes : MonoBehaviour {
+    public GameObject leftEye;
+    public GameObject rightEye;
+    public bool isRed;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    //public IEnumerator RedEyes(bool flash = false) {
+
+    //}
+}
