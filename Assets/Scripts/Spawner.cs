8edf4f87b20c216cc45d60d259c9b13b90ab6013
Clement Abel <clementan25@gmail.com>
2018-05-31 18:12:19 -0400

    Spawner Script

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+    public float spawnLimit;
+    public float spawnTimer;
+    public bool startSpawn;
+    public float itemsSpawned;
+
+    public float spawnRadius; // spawn in here
+    public Vector3 oiriginPoint;
+    public Vector3 minPos;
+    public Vector3 maxPos;
+
+    public float itemRadius; // distance to avoid per pill
+    public List<GameObject>items;
+
+    public bool useWave;
+    public int waveCount;
+    public int perWave;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Object() {
+
+    }
+
+    public void Wave() {
+
+    }
+}
